50938d0b6d07c2abdffe64cb17bab8faa60b43ff
barleenkr <barleenkr.work@gmail.com>
2026-05-03 00:11:58 +0530

    fix api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({
+export const API = axios.create({
   baseURL: "https://bioprocess-scaleup-ai.onrender.com"
 });
 
-export const predictProcess = (data) => API.post("/predict", data);
+export const predictProcess = (data) =>
+  API.post("/predict", data);
